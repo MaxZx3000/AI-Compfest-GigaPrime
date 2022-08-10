f3db6cb101bd1c4aec474c28c0cce82666a22013
MaxZx3000 <programist851@gmail.com>
2022-08-10 15:05:56 +0700

    Move AI Methods from NewsFetch to ContentBasedRecommendationAPI.

--- backend/travelling_place_project/travelling_place_api/views.py
+++ backend/travelling_place_project/travelling_place_api/views.py
@@ -11,7 +11,6 @@ import pandas as pd
 
 # Create your views here.
 # News API
-
 class PreprocessingTemplate():
     @staticmethod
     def convert_queryset_data_to_df(queryset_data, fields):
@@ -26,7 +25,6 @@ class PreprocessingTemplate():
             description = row[fields]
             tokenized_sentence = indo_lemmatizer.lemmatize(description)
             tokenized_descriptions.append(tokenized_sentence)
-    
 
 class NewsFetchLinksAPI():
     def get(self, request):
@@ -34,37 +32,8 @@ class NewsFetchLinksAPI():
         pass
 
 class NewsFetchDetailsAPI(APIView):
-    def get_descriptions(sample_description):
-        descriptions = dataset[tokenized_description_field]
-        
-        indonesian_stopwords = stopwords.words("indonesian")
-        tf_idf_vectorizer = TfidfVectorizer(stop_words = indonesian_stopwords)
-        vector_components = tf_idf_vectorizer.fit_transform(descriptions)
-        sample_description_vector_component = tf_idf_vectorizer.transform([sample_description])
-        index_to_word_mapping = tf_idf_vectorizer.get_feature_names()
-        
-        return vector_components, sample_description_vector_component, index_to_word_mapping
-    
-    def _recommend_travelling_places_using_knn(data_df, sample_description):
-        description_vector_components, sample_description_vector_component, index_to_word_mapping = get_descriptions(sample_description)
-
-        nearest_neighbors = NearestNeighbors(n_neighbors = 10)
-        nearest_neighbors.fit(description_vector_components)
-        k_nearest_neighbors_scores = nearest_neighbors.kneighbors(sample_description_vector_component)
-        
-        return k_nearest_neighbors_scores
-
-    def _get_top_n_recommendations_based_on_similarity_scores(df, top_n_indexes):
-        top_n_df = df.iloc[top_n_indexes]
-        return top_n_df
-
     def get(self, request):
         data = request.data
-        travelling_place_id = data["tourism_place_id"]
-        descriptions = self._get_descriptions()
-        nearest_neighbors = NearestNeighbors(n_neighbors = 2)
-        travelling_place nearest_neighbors.fit()
-        # travelling_place_name = TravellingPlaces().objects.qu
         pass
 
 class NewsKeywordExtractionAPI(APIView):
@@ -72,19 +41,35 @@ class NewsKeywordExtractionAPI(APIView):
         data = request.data
         pass
 
-class TourismAPI(APIView):
-    def get(self, request):
-        data = request.data
-        search_query = data["search_query"]
-        results = TravellingPlaces.objects.all()
-        NearestNeigh
-
-        pass
-
 # Content Based Filtering
 class ContentBasedRecommendationAPI():
+    def get_descriptions(self, data_df, sample_description):        
+        indonesian_stopwords = stopwords.words("indonesian")
+        tf_idf_vectorizer = TfidfVectorizer(stop_words = indonesian_stopwords)
+        vector_components = tf_idf_vectorizer.fit_transform(data_df)
+        sample_description_vector_component = tf_idf_vectorizer.transform([sample_description])
+        index_to_word_mapping = tf_idf_vectorizer.get_feature_names()
+        
+        return vector_components, sample_description_vector_component, index_to_word_mapping
+    
+    def _recommend_travelling_places_using_knn(self, vector_components, sample_description_vector_component):
+        nearest_neighbors = NearestNeighbors(n_neighbors = 10)
+        nearest_neighbors.fit(vector_components)
+        k_nearest_neighbors_scores = nearest_neighbors.kneighbors(sample_description_vector_component)
+
+        return k_nearest_neighbors_scores
+
+    def _get_top_n_recommendations_based_on_similarity_scores(self, df, top_n_indexes):
+        top_n_df = df.iloc[top_n_indexes]
+        return top_n_df
+
     def get(self, request):
         data = request.data
+        query_result = data["query"]
+        travelling_places_query_set = TravellingPlaces.objects.all()
+        data_df = PreprocessingTemplate.convert_queryset_data_to_df(travelling_places_query_set)
+        print(data_df)
+         
         pass
 
 # Colab Based Filtering
