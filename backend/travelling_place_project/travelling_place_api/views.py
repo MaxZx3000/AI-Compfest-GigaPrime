533366e0e2fa9bec180aac12ced3209cadac2aa7
MaxZx3000 <programist851@gmail.com>
2022-08-10 11:08:17 +0700

    Define Skeleton of API

--- backend/travelling_place_project/travelling_place_api/views.py
+++ backend/travelling_place_project/travelling_place_api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 # Create your views here.
+# News API
+class NewsFetch(APIView):
+    def get(self, request):
+        pass
+
+class NewsTopicModelling(APIView):
+    def get(self, request):
+        pass
+
+class TourismDescription(APIView):
+    def get(self, request):
+        pass
+
+# Content Based Filtering
+class ContentBasedRecommendationTourismPlace():
+    def get(self, request):
+        pass
+
+# Colab Based Filtering
+class ColabBasedRecommedationTourismPlace():
+    def get(self, request):
+        pass
