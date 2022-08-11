0fd0462881ac3773dd88096c24bad2c105d34be1
MaxZx3000 <programist851@gmail.com>
2022-08-11 11:14:55 +0700

    Added Fetch from json search api function

--- backend/travelling_place_project/travelling_place_api/templates/google_link_fetch.py
+++ backend/travelling_place_project/travelling_place_api/templates/google_link_fetch.py
@@ -13,3 +13,13 @@ class GoogleLinkFetch():
 
             link_urls.append(link)
             print(link)
+
+    def fetch_json_from_search_api(self, query):
+        BASE_LINK = "https://www.googleapis.com/customsearch/v1"
+        CX_KEY = "e34228993d08f4238"
+        API_KEY = "AIzaSyAsQSjiOTOUOqz-oRrp71bdmHmoa9eLqyc"
+        google_url = f"{BASE_LINK}?key={API_KEY}&cx={CX_KEY}&q={query}"
+        r = requests.get(google_url)
+        json_response = json.loads(r.text)
+
+        return json_response
