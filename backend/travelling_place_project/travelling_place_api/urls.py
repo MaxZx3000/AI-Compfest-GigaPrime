046a79af9ed23dc1f6c918b111123713707d4504
MaxZx3000 <programist851@gmail.com>
2022-08-11 11:32:50 +0700

    Added Path for NewsFetchLinksAPI.

--- backend/travelling_place_project/travelling_place_api/urls.py
+++ backend/travelling_place_project/travelling_place_api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .views import ContentBasedRecommendationAPI
+from .views import ContentBasedRecommendationAPI, NewsFetchLinksAPI
 from rest_framework import routers
 from django.urls import path, include
 
 urlpatterns = [
     path(r'travelling_places_list', ContentBasedRecommendationAPI.as_view(), name = "list_of_travelling_places"),
+    path(r'news_list', NewsFetchLinksAPI, name = "news_list"),
 ]
