ba90138abc305d60e4750e7837a91a1c4a739a6d
MaxZx3000 <programist851@gmail.com>
2022-08-12 22:02:19 +0700

    Added Splash Screen Route.

---
+++ aplikasi/lib/globals/route.dart
@@ -0,0 +1,11 @@
+import 'package:travelling_app/pages/splash_screen_page.dart';
+
+const initialRouteName = "/";
+const splashScreenRouteName = "/splash-screen";
+const homeRouteName = "/home";
+
+var routes = {
+  // initialRouteName: (context) => const HomePage(),
+  splashScreenRouteName: (context) => const SplashScreenPage(),
+  // homeRouteName: (context) => const HomePage(),
+};
