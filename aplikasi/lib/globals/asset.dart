806f6d985d678996562d084aff4a687c2610d882
MaxZx3000 <programist851@gmail.com>
2022-08-13 14:11:57 +0700

    Modify logo and splash screen background

--- aplikasi/lib/globals/asset.dart
+++ aplikasi/lib/globals/asset.dart
@@ -1,4 +1,4 @@
 const assets = <String, String>{
   'app_logo': "assets/images/logo.png",
-  "splash_screen_background": "assets/images/ss_background.png",
+  "splash_screen_background": "assets/images/ss-background.png",
 };
