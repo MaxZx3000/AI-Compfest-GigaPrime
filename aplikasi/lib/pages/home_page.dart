034026aa608d6d02f1e165748ecbd2ee36a5fe13
MaxZx3000 <programist851@gmail.com>
2022-08-13 15:01:09 +0700

    Added Search Bar Element

--- aplikasi/lib/pages/home_page.dart
+++ aplikasi/lib/pages/home_page.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:travelling_app/pages/search_bar.dart';
 
 class HomePage extends StatelessWidget{
   const HomePage({Key? key}) : super(key: key);
 
+  Function onSearchBarClick(){
+    return (){
+      print("On Search Clicked!!!");
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+        body: Stack(
+          children: [
+            SearchBar(
+                onSearchClick: onSearchBarClick,
+                maxWidth: 1000,
+                searchBarShadow: const BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(0, 5),
+                  blurRadius: 10.0,
+                  spreadRadius: 0.0,
+                )
+            )
+          ],
+        )
+    );
   }
 }
