771fa90c5decd8f40b4ae808dfcac6a195555e15
MaxZx3000 <programist851@gmail.com>
2022-08-13 14:47:31 +0700

    Added Light Orange to Orange Gradient

--- aplikasi/lib/globals/gradient.dart
+++ aplikasi/lib/globals/gradient.dart
@@ -2,6 +2,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:travelling_app/globals/colors.dart';
 
 class CustomGradient{
+  static LinearGradient getOrangeToDarkOrange({
+    Alignment beginAlignment = Alignment.topCenter,
+    Alignment endAlignment = Alignment.bottomCenter
+  }){
+    return LinearGradient(
+      begin: beginAlignment,
+      end: endAlignment,
+      colors: [
+        Color(colors['orange'] as int),
+        Color(colors['dark_orange'] as int),
+      ],
+    );
+  }
+
   static LinearGradient getLightBlueToDarkGreen({
     Alignment beginAlignment = Alignment.topCenter,
     Alignment endAlignment = Alignment.bottomCenter
