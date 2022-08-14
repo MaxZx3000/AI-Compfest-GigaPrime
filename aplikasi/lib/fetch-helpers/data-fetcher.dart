5c1a7c3f3f5a5828b0d5280ee3501c1f09cd71f9
MaxZx3000 <programist851@gmail.com>
2022-08-14 22:47:26 +0700

    Added Iteration for looping at getTravellingPlaces Data

--- aplikasi/lib/fetch-helpers/data-fetcher.dart
+++ aplikasi/lib/fetch-helpers/data-fetcher.dart
@@ -1,8 +1,17 @@
+import 'dart:convert';
+
+import 'package:travelling_app/classes/travelling_place.dart';
 import 'package:travelling_app/fetch-helpers/http-helpers.dart';
 
 class DataFetcher{
   static void getTravellingPlaces(String query) async {
     final response = await HttpHelpers.fetchTravellingPlacesList(query);
-    print(response);
+    dynamic jsonDecoded = jsonDecode(response.body);
+    List<TravellingPlace> travellingPlaces = [];
+    jsonDecoded.forEach((element) {
+      TravellingPlace travellingPlace = TravellingPlace.setFromJSON(element);
+      travellingPlaces.add(travellingPlace);
+    });
+    print(travellingPlaces);
   }
 }
