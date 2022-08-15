dba951d67eebd3bebf0e886c0453b1520a4d8efd
MaxZx3000 <programist851@gmail.com>
2022-08-15 23:39:33 +0700

    Added Border Radius in Container

--- aplikasi/lib/templates/card_template.dart
+++ aplikasi/lib/templates/card_template.dart
@@ -17,18 +17,26 @@ class CardTemplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+
         child: Column(
           children: [
             Container(
               width: double.infinity,
-              decoration:
-              BoxDecoration(gradient: CustomGradient.getLightBlueToDarkGreen()),
+              decoration: BoxDecoration(
+                gradient: CustomGradient.getOrangeToDarkOrange(),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular(15.0),
+                ),
+              ),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
                   title,
                   textAlign: TextAlign.center,
                   style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
                     color: Colors.white,
                   ),
                 ),
