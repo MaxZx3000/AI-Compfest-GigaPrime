2025e69b216892fd38368b0e67fdcbcb63436f86
MaxZx3000 <programist851@gmail.com>
2022-08-15 14:25:41 +0700

    Added Route to Detail Page

--- aplikasi/lib/globals/route.dart
+++ aplikasi/lib/globals/route.dart
@@ -1,11 +1,14 @@
-import 'package:travelling_app/pages/home_page.dart';
+import 'package:travelling_app/pages/detail/detail_page.dart';
+import 'package:travelling_app/pages/home/home_page.dart';
 import 'package:travelling_app/pages/splash_screen_page.dart';
 
 const initialRouteName = "/";
 const splashScreenRouteName = "/splash-screen";
 const homeRouteName = "/home";
+const detailRouteName = "/detail";
 
 var routes = {
   splashScreenRouteName: (context) => const SplashScreenPage(),
   homeRouteName: (context) => HomePage(),
+  detailRouteName: (context) => DetailPage(),
 };
