052847dd05c20b1ce1d71ac0b35a21ea61b17eed
MaxZx3000 <programist851@gmail.com>
2022-08-15 14:31:19 +0700

    Move file to folder pages/detail

---
+++ aplikasi/lib/pages/detail/detail_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:travelling_app/classes/travelling_place.dart';
+import 'package:travelling_app/pages/detail/detail_app_bar.dart';
+import 'package:travelling_app/utils/context.dart';
+
+class DetailPage extends StatelessWidget{
+  late TravellingPlace travellingPlace;
+
+  DetailPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var arguments = ContextUtils.getArguments(context);
+    TravellingPlace travellingPlace = arguments as TravellingPlace;
+    print(travellingPlace.placeName);
+
+    return Scaffold(
+      appBar: DetailAppBar(
+        appBarTitle: "Travelling Place Detail",
+      ).getInstance(context),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+              travellingPlace.placeName
+          ),
+        ],
+      ),
+    );
+  }
+}
