eb039a5d42a0f4d3b53eb1182e0ce6748867b96c
MaxZx3000 <programist851@gmail.com>
2022-08-18 11:39:04 +0700

    Added colab based travelling places route.

--- backend/travelling_place_project/travelling_place_api/urls.py
+++ backend/travelling_place_project/travelling_place_api/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .views import ContentBasedRecommendationAPI, NewsFetchDetailsAPI, NewsFetchLinksAPI
+from .views import ColabBasedRecommedationAPI, ContentBasedRecommendationAPI, NewsFetchDetailsAPI, NewsFetchLinksAPI
 from rest_framework import routers
 from django.urls import path, include
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path(r'travelling_places_list', ContentBasedRecommendationAPI.as_view(), name = "list_of_travelling_places"),
     path(r'news_list', NewsFetchLinksAPI.as_view(), name = "news_list"),
     path(r'news_details', NewsFetchDetailsAPI.as_view(), name = "news_details"),
+    path(r'colab_filtering_travelling_places', ColabBasedRecommedationAPI.as_view(), name = "colab_filtering_travelling_places"),
 ]
