7534f841271cd0f1c6a2b9a10c448be908be57dc
MaxZx3000 <programist851@gmail.com>
2022-08-18 11:23:57 +0700

    Added news page route

--- aplikasi/lib/globals/route.dart
+++ aplikasi/lib/globals/route.dart
@@ -1,14 +1,17 @@
 import 'package:travelling_app/pages/detail/detail_page.dart';
 import 'package:travelling_app/pages/home/home_page.dart';
+import 'package:travelling_app/pages/news/news_page.dart';
 import 'package:travelling_app/pages/splash_screen_page.dart';
 
 const initialRouteName = "/";
 const splashScreenRouteName = "/splash-screen";
 const homeRouteName = "/home";
 const detailRouteName = "/detail";
+const newsRouteName = "/news";
 
 var routes = {
   splashScreenRouteName: (context) => const SplashScreenPage(),
   homeRouteName: (context) => HomePage(),
   detailRouteName: (context) => DetailPage(),
+  newsRouteName: (context) => const NewsPage(),
 };
