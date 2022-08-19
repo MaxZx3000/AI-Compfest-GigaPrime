77111a184e774d14444aa310d2971a9721cfebb4
MaxZx3000 <programist851@gmail.com>
2022-08-19 22:45:58 +0700

    Added initBox function

--- aplikasi/lib/database/travelling_place_bookmark_db.dart
+++ aplikasi/lib/database/travelling_place_bookmark_db.dart
@@ -1,13 +1,31 @@
+import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:travelling_app/classes/travelling_place.dart';
-import '../classes/user/bookmarked_travelling_place.dart';
+import 'package:travelling_app/classes/user/bookmarked_travelling_place.dart';
 
 class TravellingPlaceBookmarkDB{
 
   late Box travellingPlaceBox;
 
-  TravellingPlaceBookmarkDB(){
-    travellingPlaceBox = Hive.box("travelling_place_bookmark");
+  Future<void> initBox() async{
+    String travellingPlaceBoxName = "travelling_place_bookmark";
+    if (!kIsWeb && !Hive.isBoxOpen(travellingPlaceBoxName)) {
+      Hive.init((await getApplicationDocumentsDirectory()).path);
+    }
+    try{
+      Hive.registerAdapter(BookmarkedTravellingPlaceAdapter());
+    }
+    on HiveError catch (_, error){
+      print("BookmarkedTravellingPlaceAdapter Adapter has been registered!");
+    }
+    try{
+      Hive.registerAdapter(TravellingPlaceAdapter());
+    }
+    on HiveError catch (_, error){
+      print("TravellingPlaceAdapter Adapter has been registered!");
+    }
+    travellingPlaceBox = await Hive.openBox(travellingPlaceBoxName);
   }
 
   void putBookmark(int rating, TravellingPlace travellingPlace) async{
@@ -16,9 +34,10 @@ class TravellingPlaceBookmarkDB{
         travellingPlace: travellingPlace
     );
 
-    bookmarkedTravellingPlace.save();
     String key = bookmarkedTravellingPlace.travellingPlace.placeId.toString();
+    print("Key: ${key}");
     await travellingPlaceBox.put(key, bookmarkedTravellingPlace);
+    print("Your data has been saved!");
   }
 
   Future<BookmarkedTravellingPlace> getOneBookmarkByKey(String placeIdKey) async{
