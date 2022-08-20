7251ae008b1a1d7e8d7fd95c3b3d2dc34b0a2aef
MaxZx3000 <programist851@gmail.com>
2022-08-20 23:03:54 +0700

    Added custom widget width

--- aplikasi/lib/templates/information_widget.dart
+++ aplikasi/lib/templates/information_widget.dart
@@ -5,41 +5,47 @@ import 'package:travelling_app/globals/gradient.dart';
 class InformationWidget extends StatelessWidget{
   final IconData iconData;
   final String information;
+  final double widgetWidth;
 
   const InformationWidget({
     Key? key,
     required this.iconData,
-    required this.information}
+    required this.information,
+    required this.widgetWidth}
   ) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(10.0),
-          height: 100,
-          width: 100,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            gradient: CustomGradient.getLightBlueToDarkGreen(
-              beginAlignment: Alignment.bottomCenter,
-              endAlignment: Alignment.topCenter,
+    return SizedBox(
+      width: widgetWidth,
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: CustomGradient.getLightBlueToDarkGreen(
+                beginAlignment: Alignment.bottomCenter,
+                endAlignment: Alignment.topCenter,
+              ),
+            ),
+            child: Icon(
+              iconData,
+              size: 70,
+              color: Colors.white,
             ),
           ),
-          child: Icon(
-            iconData,
-            size: 70,
-            color: Colors.white,
+          const SizedBox(
+            height: 15,
           ),
-        ),
-        const SizedBox(
-          height: 15,
-        ),
-        Text(
-          information
-        )
-      ],
+          Text(
+            information,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
     );
   }
 }
