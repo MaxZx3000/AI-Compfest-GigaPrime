614373a39cfe8f94aacceac772a805aad38c1eb2
MaxZx3000 <programist851@gmail.com>
2022-08-20 23:01:28 +0700

    Added convert class data to json

--- aplikasi/lib/classes/user/bookmarked_travelling_place.dart
+++ aplikasi/lib/classes/user/bookmarked_travelling_place.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:hive/hive.dart';
 import 'package:travelling_app/classes/travelling_place.dart';
 
@@ -15,4 +17,29 @@ class BookmarkedTravellingPlace{
     required this.rating,
     required this.travellingPlace,
   });
+
+  Map<String, dynamic> convertForColabJSON(){
+    return {
+      "user_id": 200001,
+      "place_rating": rating,
+      "place_id": travellingPlace.placeId,
+    };
+  }
+
+  Map<String, dynamic> convertToJSON(){
+    return {
+      "user_rating": rating,
+      "place_id": travellingPlace.placeId,
+      "place_name": travellingPlace.placeName,
+      "description": travellingPlace.description,
+      "category": travellingPlace.category,
+      "city": travellingPlace.city,
+      "price": travellingPlace.price,
+      "average_rating": travellingPlace.rating,
+      "time_minutes": travellingPlace.timeMinutes,
+      "latitude": travellingPlace.latitude,
+      "longitude": travellingPlace.longitude,
+      "summarized_description": travellingPlace.summarizedDescription,
+    };
+  }
 }
