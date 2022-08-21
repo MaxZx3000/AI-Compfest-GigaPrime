66cbc8e3e34b1a57117264f9acd0b0c6065f8cbf
MaxZx3000 <programist851@gmail.com>
2022-08-22 00:46:12 +0700

    Added Recommendation Widget

--- aplikasi/lib/pages/bookmark/recommendations_colab_widget.dart
+++ aplikasi/lib/pages/bookmark/recommendations_colab_widget.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:travelling_app/classes/travelling_place.dart';
 import 'package:travelling_app/classes/user/bookmarked_travelling_place.dart';
 import 'package:travelling_app/fetch-helpers/data-fetcher.dart';
+import 'package:travelling_app/globals/colors.dart';
+import 'package:travelling_app/globals/route.dart';
+import 'package:travelling_app/templates/card_template.dart';
+import 'package:travelling_app/templates/circular_loading_element.dart';
+import 'package:travelling_app/templates/horizontal_item_view.dart';
+import 'package:travelling_app/templates/information_widget.dart';
 
 class RecommendationColabWidget extends StatefulWidget{
 
@@ -18,24 +26,77 @@ class RecommendationColabWidget extends StatefulWidget{
 
 class _RecommendationState extends State<RecommendationColabWidget>{
 
-  Widget _getTitle(){
-    return const Text(
-      "Rekomendasi berdasarkan rating Anda.",
-      style: TextStyle(
-        fontSize: 20,
+  Widget _getLoadingWidget(){
+    return const CircularLoadingElement(
+      message: "Sedang memproses rekomendasi untuk Anda...",
+    );
+  }
+
+  Widget _getRecommendationsWidget(List<TravellingPlace> recommendedTravellingPlaces){
+    return SizedBox(
+      width: double.infinity,
+      height: 150,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        primary: false,
+        itemCount: recommendedTravellingPlaces.length,
+          itemBuilder: (context, index) {
+            TravellingPlace recommendedTravellingPlace = recommendedTravellingPlaces[index];
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: (){
+                  Navigator.pushNamed(
+                    context,
+                    detailRouteName,
+                    arguments: recommendedTravellingPlace
+                  );
+                },
+                child: HorizontalItemWidget(
+                  titleText: recommendedTravellingPlace.placeName,
+                  subtitleText: recommendedTravellingPlace.city,
+                  width: 300.0,
+                  rating: recommendedTravellingPlace.rating.toString()
+                ),
+              ),
+            );
+          },
+      ),
+    );
+  }
+
+  Widget _getNotEnoughRatingCountWidget(){
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: InformationWidget(
+        iconData: Icons.place,
+        information: "Anda memerlukan minimal 6 rating untuk mendapatkan rekomendasi.",
+        widgetWidth: 300,
       ),
     );
   }
 
   Widget _getRecommendationsContent(){
+    if (widget.bookmarkedTravellingPlaces.length < 6){
+      return _getNotEnoughRatingCountWidget();
+    }
     return FutureBuilder(
       future: DataFetcher.getColabTravellingPlaces(
         widget.bookmarkedTravellingPlaces,
       ),
       builder: (context, snapshot) {
-        print("Printing data...");
-        return Column(
-
+        if (snapshot.connectionState != ConnectionState.done){
+          return _getLoadingWidget();
+        }
+        if (snapshot.hasError){
+          print(snapshot.error);
+          return const Text(
+            "Unknown Error Occured!",
+          );
+        }
+        List<TravellingPlace> recommendedTravellingPlaces = snapshot.data as List<TravellingPlace>;
+        return _getRecommendationsWidget(
+            recommendedTravellingPlaces,
         );
       },
     );
@@ -43,11 +104,10 @@ class _RecommendationState extends State<RecommendationColabWidget>{
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _getTitle(),
-        _getRecommendationsContent()
-      ],
+    return CardTemplate(
+      height: 175,
+      title: "Rekomendasi berdasarkan rating Anda",
+      contentWidget: _getRecommendationsContent(),
     );
   }
 }
