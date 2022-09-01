d9a408b4b7f0d9fe644acf0ef9fc137c3ade691a
Max Zx <programist851@gmail.com>
2022-09-01 23:45:37 +0700

    Modify keyword extraction code method

--- backend/travelling_place_project/travelling_place_api/templates/keyword_extraction.py
+++ backend/travelling_place_project/travelling_place_api/templates/keyword_extraction.py
@@ -1,6 +1,9 @@
+from pprint import PrettyPrinter
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
 import numpy as np
+
+from backend.travelling_place_project.travelling_place_api.templates.pretty_print import PrettyPrint
 
 class KeywordExtraction:
     def _get_top_words_from_range_of_topics(self, model, feature_names, n_top_words = 10):
@@ -19,20 +22,30 @@ class KeywordExtraction:
         # print(highest_relevant_topic_index)
         return highest_relevant_topic_index
 
-    def extract_keywords(self, sentences, stopwords = [], n_components = 10):
-        tf_idf_vectorizer = TfidfVectorizer(stop_words = stopwords, norm = 'l1')
-        x = tf_idf_vectorizer.fit_transform([sentences])
-        feature_names = tf_idf_vectorizer.get_feature_names()
+    # def extract_keywords(self, sentences, stopwords = [], n_components = 10):
+    #     tf_idf_vectorizer = TfidfVectorizer(stop_words = stopwords, norm = 'l1')
+    #     x = tf_idf_vectorizer.fit_transform([sentences])
+    #     feature_names = tf_idf_vectorizer.get_feature_names()
 
-        nmf = NMF(
-            n_components = n_components,
-            max_iter = 600,
-            random_state = 10,
-            beta_loss = "kullback-leibler",
-            solver = 'mu'
-        )
+    #     nmf = NMF(
+    #         n_components = n_components,
+    #         max_iter = 600,
+    #         random_state = 10,
+    #         beta_loss = "kullback-leibler",
+    #         solver = 'mu'
+    #     )
 
-        nmf.fit(x)
-        top_topic_indexes = self._get_most_relevant_topic_from_sentences(nmf, x)
-        top_words = self._get_top_words_from_range_of_topics(nmf, feature_names)
-        return top_words[top_topic_indexes]
+    #     nmf.fit(x)
+    #     top_topic_indexes = self._get_most_relevant_topic_from_sentences(nmf, x)
+    #     top_words = self._get_top_words_from_range_of_topics(nmf, feature_names)
+    #     return top_words[top_topic_indexes]
+
+    def perform_keyword_extraction(self, ml_model, vectorizer_model, sample_text):
+        pretty_print_text = PrettyPrint.pretty_print_text(sample_text)
+        x = vectorizer_model.transform([pretty_print_text])
+        feature_names = vectorizer_model.get_feature_names()
+
+        top_topic_index = self.get_most_relevant_topic_from_sentences(ml_model, x)
+        top_words = self.get_top_words_from_range_of_topics(ml_model, feature_names)
+
+        return top_words[top_topic_index]
